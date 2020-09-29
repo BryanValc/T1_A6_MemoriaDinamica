bd4fef40e6fa390e745d7c81315eaa6e908b4eaa
Bryan Ezequiel Valdez Calderón <bryan501th@gmail.com>
2020-09-28 20:25:26 -0500

    Se creó la interfaz validación y se implementó en la clase RegistroAspirantes

--- src/PruebaMemoriaDinamica.java
+++ src/PruebaMemoriaDinamica.java
@@ -1,9 +1,32 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Scanner;
 
+interface Validacion{
+	Scanner input = new Scanner(System.in);
+	public static int validacionNatural() {
+		int ret = 0;
+		boolean err = false;
+		do {
+			try {
+				ret = input.nextInt();
+			} catch (java.util.InputMismatchException e) {
+				System.out.println("entrada no valida, intente de nuevo:");
+				input.nextLine();
+				err=true;
+			}
+			if (ret>0) {
+				err=false;
+			}else {
+				System.out.println("solo números naturales");
+				err=true;
+			}
+		}while(err);
+		return ret;
+	}
+}
 class Aspirante{
-	
 	//folio
 	private String Nombre;
 	private byte edad;
@@ -32,45 +55,38 @@ class Aspirante{
 	public void setRedesSociales(String[] redesSociales) {
 		this.redesSociales = redesSociales;
 	}
+	
 	@Override
 	public String toString() {
 		return "Aspirante [Nombre=" + Nombre + ", edad=" + edad + ", redesSociales=" + Arrays.toString(redesSociales)
 				+ "]";
 	}
+	
 }//class Aspirante
 
 
-class RegistroAspirantes{
+class RegistroAspirantes implements Validacion{
 	
 	ArrayList listaAspirantes;
 	
 	public RegistroAspirantes() {
 		listaAspirantes = new ArrayList();
 	}
-	
 	public void agregarAspirante(Aspirante a) {
 		listaAspirantes.add(a);
 	}
-	
 	public Object eliminarAspirante(String folio) {
-		
 		//eliminacion del ultimo aspirante
 		return listaAspirantes.remove(listaAspirantes.size()-1);
 	}
-	
 	public void mostrarAspirantes() {
-		
 		//Recorrido de un ARRAYLIST
-		
 		System.out.println("----Forma 1 con un objeto ITERATOR--------");
 		Iterator i = listaAspirantes.iterator();
 		while(i.hasNext()) {
 			Aspirante a = (Aspirante) i.next();
 			System.out.println(a);
 		}
-		
-		
-		
 	}	
 }
 
@@ -78,14 +94,10 @@ public class PruebaMemoriaDinamica {
 	public static void main(String[] args) {
 		
 		RegistroAspirantes ra = new RegistroAspirantes();
-		
+	
 		String redes[] = {"FB", "TW", "IG"};
 		
 		ra.agregarAspirante(new Aspirante("1", (byte)1, redes));
-		ra.agregarAspirante(new Aspirante("2", (byte)2, redes));
-		ra.agregarAspirante(new Aspirante("3", (byte)3, redes));
-		ra.agregarAspirante(new Aspirante("4", (byte)4, redes));
-		ra.agregarAspirante(new Aspirante("5", (byte)5, redes));
 		
 		ra.mostrarAspirantes();
 		
